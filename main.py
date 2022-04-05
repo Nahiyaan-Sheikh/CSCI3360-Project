021933358e90d62fd3477201e32619bcf7c74c11
zekesiegman <eas14751@uga.edu>
2022-04-05 17:12:13 -0400

    added gpu data file

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib as mp
 
-gpu_df = pd.read_csv('./GPU data/FACT_GPU_PRICE.csv')
+gpu_df = pd.read_csv('./gpu/FACT_GPU_PRICE.csv')
 
 # drop unused columns
 to_drop = ['RegionId', 'MerchantId']
@@ -10,12 +10,6 @@ gpu_df.drop(to_drop, inplace=True, axis=1)
 
 
 # drop unused rows
-rx = range(893, 945)
-gtx960 = range(1879, 1898)
-gtx970_980 = range(1907, 1948)
-gtx10 = range(995, 1108)
-titan = range(1960, 1975)
-
 datarx = gpu_df[gpu_df['ProdId'] >= 893]
 datarx = datarx[datarx['ProdId'] <= 945]
 
@@ -36,3 +30,4 @@ frames = [datarx, data960, data970_80, data10, datatitan]
 gpu_final = pd.concat(frames)
 
 print(gpu_final)
+
